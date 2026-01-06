670aee0a302cc2e4d230008e43320314243185f2
고용민 <june18806@gmail.com>
2026-01-06 17:35:31 +0900

    [level 0] Title: 특정 문자열로 끝나는 가장 긴 부분 문자열 찾기, Time: 0.01 ms, Memory: 10.3 MB -BaekjoonHub

---
+++ 프로그래머스/0/181872. 특정 문자열로 끝나는 가장 긴 부분 문자열 찾기/특정 문자열로 끝나는 가장 긴 부분 문자열 찾기.py
@@ -0,0 +1,11 @@
+def solution(myString, pat):
+    
+    for i in range(len(myString)):
+        if myString[i]==pat[-1]:
+            answer=myString[:i+1]
+    
+    
+            
+        
+    
+    return answer
