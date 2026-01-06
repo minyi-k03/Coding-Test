9e9943da8f2ac56b3d67018c36015ad65837f93e
고용민 <june18806@gmail.com>
2026-01-06 17:44:30 +0900

    [level 0] Title: 영어가 싫어요, Time: 0.03 ms, Memory: 10.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/120894. 영어가 싫어요/영어가 싫어요.py
@@ -0,0 +1,8 @@
+def solution(numbers):
+    answer = []
+    nums=["zero","one","two","three","four","five","six","seven","eight","nine"]
+    
+    for idx,num in enumerate(nums):
+        numbers=numbers.replace(num,str(idx))
+   
+    return int(numbers)
