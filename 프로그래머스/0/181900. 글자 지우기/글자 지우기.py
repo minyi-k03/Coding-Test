143a323be9760f76485c69b93bdde50ef824a3de
고용민 <june18806@gmail.com>
2026-01-06 17:33:12 +0900

    [level 0] Title: 글자 지우기, Time: 0.09 ms, Memory: 10.3 MB -BaekjoonHub

---
+++ 프로그래머스/0/181900. 글자 지우기/글자 지우기.py
@@ -0,0 +1,12 @@
+def solution(my_string, indices):
+    answer = ''
+    
+    for idx,val in enumerate(my_string):
+        if idx not in indices:
+            answer+=val
+    
+    
+    
+   
+  
+    return answer
