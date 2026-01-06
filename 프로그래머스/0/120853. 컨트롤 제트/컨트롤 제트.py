1a72bc38126bef7ce3e92f2a48e26b01516d5c03
고용민 <june18806@gmail.com>
2026-01-06 17:42:26 +0900

    [level 0] Title: 컨트롤 제트, Time: 0.05 ms, Memory: 10.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/120853. 컨트롤 제트/컨트롤 제트.py
@@ -0,0 +1,13 @@
+def solution(s):
+    s=s.split()
+    stack=[]
+    
+    for i in s:
+        if i=="Z":
+            stack.pop()
+        else:
+            stack.append(int(i))
+            
+            
+    
+    return sum(stack) 
