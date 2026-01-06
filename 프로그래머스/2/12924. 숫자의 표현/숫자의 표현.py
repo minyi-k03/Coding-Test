cfcf6636ffc54f94b6985b5d934733ccf6d843e9
고용민 <june18806@gmail.com>
2026-01-06 18:17:04 +0900

    [level 2] Title: 숫자의 표현, Time: 6.30 ms, Memory: 10.1 MB -BaekjoonHub

---
+++ 프로그래머스/2/12924. 숫자의 표현/숫자의 표현.py
@@ -0,0 +1,15 @@
+def solution(n):
+    answer = 0
+    
+    for i in range(1,n+1):
+        sum_num=0
+        for j in range(i,n+1):
+            
+            sum_num+=j
+            if sum_num==n:
+                answer+=1
+                break
+            elif sum_num>n:
+                break
+            
+    return answer
