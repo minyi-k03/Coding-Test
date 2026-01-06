78488f10016f5c513af31c8497616f052d9ccd03
고용민 <june18806@gmail.com>
2026-01-06 18:10:20 +0900

    [level 1] Title: 둘만의 암호, Time: 0.05 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/1/155652. 둘만의 암호/둘만의 암호.py
@@ -0,0 +1,12 @@
+def solution(s, skip, index):
+    answer = ''
+    alphabet=['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u',
+             'v','w','x','y','z']
+    tmp=''
+    
+    for i in skip:
+        alphabet.remove(i)
+            
+    for i in s:
+        answer+=alphabet[(alphabet.index(i)+index)%len(alphabet)]
+    return answer
